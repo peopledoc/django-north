3b63c36d80080be57642ab501702aa46e79cb53d
Lauréline Guérin <zebuline@crocobox.org>
2017-06-20 16:57:08 +0200

    anonymize some test files

--- tests/test_data/sql/17.01/manual/17.01-feature_a-040-dml.sql
+++ tests/test_data/sql/17.01/manual/17.01-feature_a-040-dml.sql
@@ -7,30 +7,30 @@ SELECT * from foo;
 
 --meta-psql:do-until-0
 
-WITH sender_data AS (
+WITH author_data AS (
     SELECT
-        document_document.id as document_id,
-        document_document.sender_id as manager_id,
-        document_document.sender_first_name as first_name,
-        document_document.sender_last_name as last_name
-    FROM document_document
-        LEFT JOIN account_manager
-            ON account_manager.user_id = document_document.sender_id
-        LEFT JOIN document_senderinfo
-            ON document_senderinfo.document_id = document_document.id
+        store_book.id as book_id,
+        store_book.author_id as author_id,
+        store_book.author_first_name as first_name,
+        store_book.author_last_name as last_name
+    FROM store_book
+        LEFT JOIN store_author
+            ON store_author.id = store_book.author_id
+        LEFT JOIN book_authorinfo
+            ON book_authorinfo.book_id = store_book.id
 
-    WHERE account_manager.user_id IS NOT NULL
-        AND document_senderinfo.document_id IS NULL
+    WHERE store_author.id IS NOT NULL
+        AND book_authorinfo.book_id IS NULL
     LIMIT 5000
 )
-INSERT INTO document_senderinfo (
-    document_id,
-    manager_id,
+INSERT INTO book_authorinfo (
+    book_id,
+    author_id,
     first_name,
     last_name
 )
-SELECT sender_data.*
-FROM sender_data;
+SELECT author_data.*
+FROM author_data;
 
 
 --meta-psql:done

ebd03f27475af81e09662570830233f102858f44
Vincent Rialland <vincent.rialland@people-doc.com>
2018-05-29 18:10:48 +0200

    Refs #31 Add required arguments

--- django_north/management/commands/migrate.py
+++ django_north/management/commands/migrate.py
@@ -6,6 +6,7 @@ import os.path
 from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.db import connection
+from django.db import DEFAULT_DB_ALIAS
 
 from django_north.management import migrations
 from django_north.management.runner import Script
@@ -15,6 +16,23 @@ logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
     help = "Migrate the DB to the target version."
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--noinput', '--no-input', action='store_false',
+            dest='interactive',
+            help='Tells Django to NOT prompt the user for input of any kind.',
+        )
+        parser.add_argument(
+            '--database', action='store', dest='database',
+            default=DEFAULT_DB_ALIAS,
+            help='Nominates a database to synchronize. '
+                 'Defaults to the "default" database.',
+        )
+        parser.add_argument(
+            '--run-syncdb', action='store_true', dest='run_syncdb',
+            help='Creates tables for apps without migrations.',
+        )
 
     def handle(self, *args, **options):
         if getattr(settings, 'NORTH_MANAGE_DB', False) is not True:

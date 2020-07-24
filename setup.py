2812b67135917bfa1c33fe4e115017011ba37365
Joachim Jablon <joachim.jablon@people-doc.com>
2020-07-24 09:34:36 +0200

    Switch to septentrion 0.6.0

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     install_requires=[
         "Django>=1.11",
         "sqlparse",
-        "septentrion[psycopg2]>=0.5.1",
+        "septentrion[psycopg2]>=0.6.0",
     ],
     tests_require=["tox"],
     license="MIT",
